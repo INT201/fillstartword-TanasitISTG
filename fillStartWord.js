36c8ca4da1e56ffd3184cad9553e70e67933beb8
TanasitISTG <tanasit.istg@mail.kmutt.ac.th>
2022-08-31 23:31:30 +0700

    test

--- fillStartWord.js
+++ fillStartWord.js
@@ -3,7 +3,9 @@ const { template } = require("@babel/core");
 function fillStartWord(startWord, word) {
   if (word === undefined || word === null) {
     console.log(undefined);
-  } else if (word.includes(startWord)) {
+  }
+  
+  if (word.includes(startWord)) {
     console.log(word);
   } else {
     console.log(startWord + word);
