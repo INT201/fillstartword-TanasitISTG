31e109b8fc5dafa51588225916ea6ef3c59f0b48
TanasitISTG <tanasit.istg@mail.kmutt.ac.th>
2022-08-31 23:24:41 +0700

    switch the if else statements

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,15 +1,23 @@
 const { template } = require("@babel/core");
 
 function fillStartWord(startWord, word) {
-  if (word === undefined || word === null) {
-    console.log(undefined);
-  } else if (word.includes(startWord)) {
+  // if (word === undefined || word === null) {
+  //   console.log(undefined);
+  // } else if (word.includes(startWord)) {
+  //   console.log(word);
+  // } else {
+  //   console.log(startWord + word);
+  // }
+
+  if (word.includes(startWord)) {
     console.log(word);
+  } else if (word === undefined || word === null) {
+    console.log(undefined);
   } else {
     console.log(startWord + word);
   }
 }
 
-fillStartWord("first", null);
+fillStartWord("1-2565-", "Hello World");
 
 module.exports = fillStartWord;
