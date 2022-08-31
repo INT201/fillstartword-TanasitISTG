6f1812da68125a54a73cfa4799524ab262a6b1e6
TanasitISTG <tanasit.istg@mail.kmutt.ac.th>
2022-08-31 23:25:44 +0700

    nah I'm so confusing right now

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,23 +1,13 @@
 const { template } = require("@babel/core");
 
 function fillStartWord(startWord, word) {
-  // if (word === undefined || word === null) {
-  //   console.log(undefined);
-  // } else if (word.includes(startWord)) {
-  //   console.log(word);
-  // } else {
-  //   console.log(startWord + word);
-  // }
-
-  if (word.includes(startWord)) {
-    console.log(word);
-  } else if (word === undefined || word === null) {
+  if (word === undefined || word === null) {
     console.log(undefined);
+  } else if (word.includes(startWord)) {
+    console.log(word);
   } else {
     console.log(startWord + word);
   }
 }
-
-fillStartWord("1-2565-", "Hello World");
 
 module.exports = fillStartWord;
